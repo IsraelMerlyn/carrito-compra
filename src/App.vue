61a86c057fc9d7a75e8a21eec3fcf1c91f027a00
IsraelMerlyn <merlyn142a@gmail.com>
2025-02-08 09:54:21 -0600

    Tasks #0.2 Agregando Data en formato Json

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,114 @@
-<script setup>
-const hola = "hola mundo"
-</script>
-
 <template>
-  <div>
-    <h1>{{ hola }}</h1>
-  </div>
+  <header class="py-5 header">
+    <div class="container-xl">
+      <div class="row justify-content-center justify-content-md-between">
+        <div class="col-8 col-md-3">
+          <a href="index.html">
+            <img class="img-fluid" src="./public/img/logo.svg" alt="imagen logo">
+          </a>
+        </div>
+        <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+          <div class="carrito">
+            <img class="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
+            <div id="carrito" class="bg-white p-3">
+              <p class="text-center">El carrito está vacío</p>
+              <table class="w-100 table">
+                <thead>
+                  <tr>
+                    <th>Imagen</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <img class="img-fluid" :src="iphone" alt="imagen guitarra">
+                    </td>
+                    <td>SRV</td>
+                    <td class="fw-bold">
+                      $399
+                    </td>
+                    <td class="flex align-items-start gap-4">
+                      <button type="button" class="btn btn-dark">
+                        -
+                      </button>
+                      1
+                      <button type="button" class="btn btn-dark">
+                        +
+                      </button>
+                    </td>
+                    <td>
+                      <button class="btn btn-danger" type="button">
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <p class="text-end">Total a pagar: <span class="fw-bold">$899</span></p>
+              <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+            </div>
+          </div>
+        </nav>
+      </div><!--.row-->
+
+      <div class="row mt-5">
+        <div class="col-md-6 text-center text-md-start pt-5">
+          <h1 class="display-2 fw-bold">Modelo VAI</h1>
+          <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus
+            quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio
+            exercitationem eos inventore odit.</p>
+          <p class="text-primary fs-1 fw-black">$399</p>
+          <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5">Agregar al Carrito</button>
+        </div>
+      </div>
+    </div>
+
+    <img class="header-guitarra" src="./public/img/header_guitarra.png" alt="imagen header">
+  </header>
+
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Celulares en Promocion</h2>
+
+    <div class="row mt-5">
+      <CardProductos v-for="celular in celulares" :key="celular.id" :celular="celular" />
+    </div>
+  </main>
+
+  <footer class="bg-dark mt-5 py-5">
+    <div class="container-xl">
+      <p class="text-white text-center fs-4 mt-4 m-md-0">IsraelMerlyn - Todos los derechos Reservados</p>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
-h1 {
-  color: brown;
+<script>
+import { ref, onMounted } from 'vue';
+import { db } from './data/celulares';
+import CardProductos from './components/CardProductos.vue';
+
+export default {
+  components: {
+    CardProductos
+  },
+  setup() {
+    const celulares = ref([]);
+
+    onMounted(() => {
+      celulares.value = db;
+      console.log('la data es', celulares.value);
+    });
+
+    return {
+      celulares
+    };
+  }
 }
-</style>
+</script>
+
+<style scoped></style>
---
+++ src/components/CardProductos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" :src="celular.imagen" alt="imagen celular">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ celular.nombre }}</h3>
+            <p>{{ celular.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">{{ `$ ${celular.precio}` }}</p>
+            <!-- Quitar filtro si no está definido -->
+            <button style="border-radius: 15px;" type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN CELULAR -->
+</template>
+
+<script>
+export default {
+    props: {
+        celular: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
